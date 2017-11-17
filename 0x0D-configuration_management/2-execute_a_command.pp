7ab669ce26c85fdbeb76a1219c7c159bf879a02f
jvpupcat <vuong.joann@yahoo.com>
2017-11-17 03:04:42 +0000

    aligned arrows

--- 0x0D-configuration_management/2-execute_a_command.pp
+++ 0x0D-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,7 @@
 # script that creates a manifest that kills a process
 
 exec { 'killmenow':
-  command => 'pkill -f ./killmenow',
-  cwd => '/root',
+  command  => 'pkill -f ./killmenow',
+  cwd      => '/root',
   provider => 'shell'
 }
