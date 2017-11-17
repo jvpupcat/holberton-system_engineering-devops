42b21a87cc479060e6a6d9ae8d4cf847ff939a25
jvpupcat <vuong.joann@yahoo.com>
2017-11-17 01:28:16 +0000

    fixed other puppet-lint issues

--- 0x0D-configuration_management/0-create_a_file.pp
+++ 0x0D-configuration_management/0-create_a_file.pp
@@ -2,6 +2,6 @@ file { '/tmp/holberton':
   ensure  => 'present',
   owner   => 'www-data',
   group   => 'www-data',
-  mode    => 744,
-  content => "I love Puppet"
+  mode    => '0744',
+  content => 'I love Puppet'
 }
