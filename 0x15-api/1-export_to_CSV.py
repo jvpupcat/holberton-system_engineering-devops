563f1d62f49342f61eed9485990225502f6c496c
jvpupcat <130@holbertonschool.com>
2018-01-03 04:58:29 +0000

    first commit

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Python script that returns info about employee TODO list progress"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    user_data = {'id': sys.argv[1]}
+    todo_data = {'userId': sys.argv[1]}
+    user_url = 'https://jsonplaceholder.typicode.com/users'
+    todo_url = 'https://jsonplaceholder.typicode.com/todos'
+
+    #user data request
+    user_request = requests.get(user_url, params=user_data)
+    user_json = user_request.json()
+    for employee in user_json:
+        username = employee['username']
+
+    #get employee todo list
+    todo_request = requests.get(todo_url, params=todo_data)
+    todo_json = todo_request.json()
+    csv_file = user_id + ".csv"
+
+    #get tasks done and title of it
+    for key in todo_json:
+        status = key.get('completed')
+        title = key.get('title')
+        with open(csv_file, 'a+') as f:
+            f.write('"{}","{}","{}","{}"\n'.format
+                    (user_id, username, status, title))
