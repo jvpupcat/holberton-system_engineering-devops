0b68008874388f6bccfbf23f332a97b24c4bd225
jvpupcat <130@holbertonschool.com>
2018-01-09 02:50:48 +0000

    used try/except

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,11 +8,14 @@ def number_of_subscribers(subreddit):
     """Queries number of subscribers for subreddit"""
     if type(subreddit) is not str:
         return 0
-    reddit_url = requests.get("https://www.reddit.com/r/{}/about.json".format
-                              (subreddit),
-                              headers={'user-agent': 'jvpupcat'},
-                              allow_redirects=False)
-    NumOfSubscribers = reddit_url.json().get('data').get('subscribers')
+    try:
+        reddit_url = requests.get("https://www.reddit.com/r/{}/about.json"
+                                  .format(subreddit),
+                                  headers={'user-agent': 'jvpupcat'},
+                                  allow_redirects=False)
+        NumOfSubscribers = reddit_url.json().get('data').get('subscribers')
+    except AttributeError:
+        return 0
     if NumOfSubscribers is None:
         return 0
     return NumOfSubscribers
