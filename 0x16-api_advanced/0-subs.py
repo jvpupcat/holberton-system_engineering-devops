e561981ecd182c3ad6e330eabc4f3efe0d40cbd3
jvpupcat <130@holbertonschool.com>
2018-01-09 00:26:41 +0000

    first commit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""function that returns # of subscribers from Reddit API"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """Queries number of subscribers for subreddit"""
+    if type(subreddit) is not str:
+        return 0
+    reddit_url = requests.get("https://www.reddit.com/r/{}/about.json".format(subreddit), headers={'user-agent': 'jvpupcat'}, allow_redirects=False)
+    NumOfSubscribers = reddit_url.json().get('data').get('subscribers')
+    if NumOfSubscribers is None:
+        return 0
+    return NumOfSubscribers
