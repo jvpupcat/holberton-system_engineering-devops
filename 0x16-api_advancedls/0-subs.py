7fb4408f0161ad164b81a4d036d45de501a14863
jvpupcat <130@holbertonschool.com>
2018-01-09 00:16:52 +0000

    fixed some syntax and spelling error

--- 0x16-api_advancedls/0-subs.py
+++ 0x16-api_advancedls/0-subs.py
@@ -2,14 +2,13 @@
 """function that returns # of subscribers from Reddit API"""
 
 import requests
-import json
 
 def number_of_subscribers(subreddit):
     """Queries number of subscribers for subreddit"""
     if type(subreddit) is not str:
         return 0
-    reddit_url = ("https://www.reddit.com/r/{}/about.json".format(subreddit), {'user-agent': 'someone153'})
-    NumOfSubcribers = requests.reddit_url.get('data').get('subscribers')
+    reddit_url = requests.get("https://www.reddit.com/dev/api/".format(subreddit), headers={'user-agent': 'jvpupcat'}, allow_redirects=False)
+    NumOfSubscribers = reddit_url.json().get('data').get('subscribers')
     if NumOfSubscribers is None:
         return 0
-    return NumOfSubscribers.json().get('data').get('subscribers')
+    return NumOfSubscribers
