3281c92d269c681d2a992d87f95398291668becc
jvpupcat <130@holbertonschool.com>
2018-01-10 13:57:23 +0000

    removed for loop

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,10 +4,16 @@
 import requests
 
 
-def recurse(subreddit, hot_list[])
-    reddit_url = requests.get("https://www.reddit.com/r/{}/hot.json"
+def recurse(subreddit, hot_list[], after)
+    reddit_hot = requests.get("https://www.reddit.com/r/{}/hot.json"
                               .format(subreddit),
                               headers={'user-agent': 'jvpupcat'},
-                              allow_redirects=False)
-    try:
-        
+                              allow_redirects=False,
+                              params=)
+
+    GetDataChildren = reddit_hot.json().get('data').get('children')
+    title = key.get('data').get('title')
+    hot_list = hot_list.append(title)
+    if after is None:
+        return hot_list
+    recurse(subreddit, hot_list, after)
