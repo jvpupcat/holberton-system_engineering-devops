85eae38e540a0e2adec49b3e585af252beb5a38f
jvpupcat <130@holbertonschool.com>
2018-01-10 15:22:48 +0000

    added after

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,16 +4,18 @@
 import requests
 
 
-def recurse(subreddit, hot_list[], after)
+def recurse(subreddit, hot_list=[], after=None):
     reddit_hot = requests.get("https://www.reddit.com/r/{}/hot.json"
                               .format(subreddit),
                               headers={'user-agent': 'jvpupcat'},
                               allow_redirects=False,
-                              params=)
+                              params={'after': after})
 
     GetDataChildren = reddit_hot.json().get('data').get('children')
-    title = key.get('data').get('title')
-    hot_list = hot_list.append(title)
+    for key in GetDataChildren:
+        title = key.get('data').get('title')
+        hot_list.append(title)
+    after = reddit_hot.json().get('data').get('after')
     if after is None:
         return hot_list
-    recurse(subreddit, hot_list, after)
+    return recurse(subreddit, hot_list, after)
