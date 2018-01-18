806953f54c80d0c39674e56bf535d40049352180
jvpupcat <130@holbertonschool.com>
2018-01-18 21:03:13 +0000

    added comments

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,4 @@
+# typo fix in wp-settings.php
 exec { 'replace phpp with php':
 	command => sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
 	path 	=> ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
