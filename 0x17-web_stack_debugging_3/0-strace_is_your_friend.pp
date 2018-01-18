7074fec31ef94beb82665e0df68b2c5f232efbbd
jvpupcat <130@holbertonschool.com>
2018-01-18 21:18:51 +0000

    removed tabs, added spaces

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # typo fix in wp-settings.php
 exec { 'replace phpp with php':
-	command => sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
-	path 	=> ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
+  command => sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
 }
