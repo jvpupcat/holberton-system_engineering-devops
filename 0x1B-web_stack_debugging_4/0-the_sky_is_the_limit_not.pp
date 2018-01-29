ec9e61b09a8795b214a1fa573156676c5f605340
jvpupcat <130@holbertonschool.com>
2018-01-29 06:32:10 +0000

    first commit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 #higher limit
-exec { 'higher limit' =
+exec { 'higher limit':
   command => "sed -i 's/15/4096' /etc/default/nginx",
   path    => '/usr/bin:/usr/sbin:/bin:/usr/local/sbin:/usr/local/bin',
 }
